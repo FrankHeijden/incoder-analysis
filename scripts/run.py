b0ceac85d618402a271acd6e6c37650baa0a497d
Frank van der Heijden <frank.boekanier@gmail.com>
2022-06-09 14:50:16 +0200

    Refactor run.py for .jsonl

--- scripts/run.py
+++ scripts/run.py
@@ -6,12 +6,9 @@ import time
 from tqdm import tqdm
 
 import incoder
-import util
 
 
-def _run_file(file: str) -> None:
-    data = json.loads(util.read_file(file))
-
+def _run_data(data: str) -> str:
     left_context_only = data["left_context_only"]
     t_bf = time.time()
     left_context_only["prediction"] = incoder.generate(left_context_only["left_context"], "")
@@ -21,21 +18,19 @@ def _run_file(file: str) -> None:
     t_bf = time.time()
     both_contexts["prediction"] = incoder.generate(both_contexts["left_context"], both_contexts["right_context"])
     both_contexts["inference_time"] = (time.time() - t_bf) * 1000
-    with open("output" + file[7:], "w") as f:
-        json.dump(data, f)
+    return data
 
 
-def run(n: int, i: int) -> None:
-    files = util.get_files_in_dir("dataset/python") + util.get_files_in_dir("dataset/javascript")
-    os.makedirs("output/python", exist_ok=True)
-    os.makedirs("output/javascript", exist_ok=True)
+def run(data_path: str, n: int, i: int) -> None:
+    os.makedirs(f"../output/{data_path}", exist_ok=True)
 
-    output_files = set(util.get_files_in_dir("output/python") + util.get_files_in_dir("output/javascript"))
-    files = list(filter(lambda f: f not in output_files and abs(hash(f)) % n == i, files))
+    with open(f"../dataset/{data_path}/data.jsonl", "r") as f:
+        data_batch = f.readlines()[i::n]
 
-    for file in tqdm(files, total=len(files)):
-        _run_file(file)
+        with open(f"../output/{data_path}/data.jsonl", "a") as f_out:
+            for data_str in tqdm(data_batch, total=len(data_batch)):
+                f_out.write(json.dumps(_run_data(json.loads(data_str))) + "\n")
 
 
 if __name__ == '__main__':
-    run(int(sys.argv[1]), int(sys.argv[2]))
+    run(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
