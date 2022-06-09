fb6d4d42e63f15497f6b5950f5f82418192173af
Frank van der Heijden <frank.boekanier@gmail.com>
2022-06-09 20:00:31 +0200

    Inference in parallel

--- scripts/run.py
+++ scripts/run.py
@@ -3,7 +3,8 @@ import os
 import sys
 import time
 
-from tqdm import tqdm
+from joblib import Parallel, delayed
+from typing import List
 
 import incoder
 
@@ -21,15 +22,33 @@ def _run_data(data: str) -> str:
     return data
 
 
+def _process_data_batch(data_batch: List[str], data_path: str, i: int, j: int) -> None:
+    batch_length = len(data_batch)
+    if batch_length == 0:
+        return
+
+    with open(f"../output/{data_path}/data-{i}-{j}.jsonl", "a") as f_out:
+        percentage_done = 0
+        for k, data_str in enumerate(data_batch):
+            percentage_done_new = int(100.0 * k / batch_length)
+            if percentage_done != percentage_done_new:
+                percentage_done = percentage_done_new
+                print(f"Batch {j}: {percentage_done}%")
+            f_out.write(json.dumps(_run_data(json.loads(data_str))) + "\n")
+
+
 def run(data_path: str, n: int, i: int) -> None:
     os.makedirs(f"../output/{data_path}", exist_ok=True)
 
     with open(f"../dataset/{data_path}/data.jsonl", "r") as f:
-        data_batch = f.readlines()[i::n]
-
-        with open(f"../output/{data_path}/data.jsonl", "a") as f_out:
-            for data_str in tqdm(data_batch, total=len(data_batch)):
-                f_out.write(json.dumps(_run_data(json.loads(data_str))) + "\n")
+        lines = f.readlines()[i::n]
+        batch_count = 4
+        Parallel(n_jobs=batch_count, verbose=1)(delayed(_process_data_batch)(
+            lines[j::batch_count],
+            data_path,
+            i,
+            j
+        ) for j in range(batch_count))
 
 
 if __name__ == '__main__':
